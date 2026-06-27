6b6161fcaa5f01df07e4b9718289949f58f3b678
exobition <116936838+exobition@users.noreply.github.com>
2026-06-27 16:13:59 -0700

    Update stage2.asm

--- src/stage2.asm
+++ src/stage2.asm
@@ -7,7 +7,10 @@ main:
   mov ds, ax
   mov es, ax
   mov ss, ax
-  mov sp, 0x7e10
+  mov sp, 0x7a0
+
+  mov [BOOT_DRIVE], dl
+
   sti
   
   mov si, msg
@@ -16,11 +19,17 @@ main:
 printf:
   lodsb
   test al, al
-  jz $
+  jz .loop
   mov ah, 0x0e
   int 0x10
   jmp printf
 
+.loop:
+  cli
+  hlt
+
 msg: db "disk read successful, stage 2 loaded", 0
+
+BOOT_DRIVE db 0
 
 times 512-($-$$) db 0

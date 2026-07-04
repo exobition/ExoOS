35a4557cbedb5624f41d9e6a5b417511aa7c24ee
exobition <116936838+exobition@users.noreply.github.com>
2026-07-04 14:54:06 -0700

    Fixing redundant code, adding screen clearing, and read desc
    adding more space to work with, by changing times 512($-$$) db 0 to 2 KiB rather than 512 bytes

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,3 +1,4 @@
+
 [bits 32]
 [org 0x10000]
 
@@ -8,44 +9,58 @@ main:
 
   mov edi, 0xB8000
   mov edx, 0xB8000
-
-  call printf
   
-  mov al, 0x00 
+  call clearVGA 
+  call printf
 
   jmp $
+
+clearVGA:
+  mov al, nullchar
+  mov ah, VGAcolor
+  cmp edi, VGAMAX
+  je resetVGA
+  mov [edi], al
+  inc edi
+  mov [edi], ah
+  inc edi
+  jmp clearVGA
+
+resetVGA:
+  mov edi, 0xB8000
+  jmp return
 
 printf:
   lodsb
   test al, al
   jz return
   cmp al, 0x0A 
-  je newline
-  mov ah, 0x0F
+  je DNSys 
+  mov ah, VGAcolor
   mov [edi], al
   mov ebx, 0x02
   add edi, ebx
   xor ebx, ebx
   jmp printf
 
-newline:
-  add edx, VGANL
-  cmp edi, edx
-  ja DNSys
-  mov edi, edx
-  jmp printf
-  
 DNSys:
   add edx, VGANL
-  jmp newline
+  cmp edi, edx
+  ja DNSys 
+  mov edi, edx
+  jmp printf
 
 return:
 
   ret
   
 section .data
-  msg: db "#########################", 10, "# Welcome to ExoOS v0.1 #", 10, "#########################", 0 
+msg: db "#########################", 10, "# Welcome to ExoOS v0.1 #", 10, "#########################", 0 
+
+  nullchar equ 0x20
+  VGAcolor equ 0x0F
 
   VGANL equ 0xA0
+  VGAMAX equ 0xB9000
 
-times 512-($-$$) db 0
+times 2048-($-$$) db 0

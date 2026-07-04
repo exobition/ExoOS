bac38793db922b144ba964a950fca95b282c8568
exobition <116936838+exobition@users.noreply.github.com>
2026-07-03 17:11:02 -0700

    adding VGA driver to kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,11 +1,42 @@
 [bits 32]
 [org 0x10000]
 
+section .text
+
 main:
-  mov al, 'A'
-  mov ah, 0x0F
-  mov [0xB8000], ax
+  mov esi, msg
+
+  mov edi, 0xB8000
+  mov edx, 0xB8000
+
+  call printf
+  
+  mov al, 0x00 
+
+  call keyboard
 
   jmp $
+
+printf:
+  lodsb
+  test al, al
+  jz return
+  cmp al, 0x0A 
+  je newline
+  mov ah, 0x0F
+  mov [edi], al
+  mov ebx, 0x02
+  add edi, ebx
+  xor ebx, ebx
+  jmp printf
+
+return:
+
+  ret
+  
+section .data
+  msg: db "#########################", 10, "# Welcome to ExoOS v0.1 #", 10, "#########################", 0 
+
+  VGANL equ 0xA0
 
 times 512-($-$$) db 0
